1eee6abf703248d83f74d50b975f69bb5f048f22
Pierrot LEMEKCHO <pierrotlemekcho@gmail.com>
2018-10-27 12:15:57 +0200

    Revert "Initial release with sqlalchemy"

---
+++ connectdb.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import psycopg2
+from config import config
+
+
+""" etablir connection POSTGRES """
+
+def connect():
+    """ connect a la base PostgreSQL """
+    conn = None
+    try :
+        # lire les paramete de connection
+        params = config()
+        print(params)        
+        # connection au serveur
+        print(' connection au serveur Postgres  ....')
+        conn = psycopg2.connect(**params)
+
+        # creer un curseur 
+        cur = conn.cursor()
+
+        # exectuter un ordre
+        print('PostgresSQL base de donne version:')
+        cur.execute('SELECT version()')
+        db_version = cur.fetchone()
+        print(db_version)
+
+	# penser a commit()
+
+        # fermer la connection
+        cur.close
+    except (Exception, psycopg2.DatabaseError) as error :
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print(' connexion a la base fermé ')
+
+if __name__ == '__main__':
+    connect()
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79
---
+++ createdb.py
@@ -0,0 +1,50 @@
+import psycopg2
+from config import config
+"""  voir explications
+	http://www.postgresqltutorial.com/postgresql-python/create-tables/
+		"""
+
+
+
+def create_tables():
+    """ create tables in the PostgreSQL database"""
+    commands = (
+        """
+        CREATE TABLE relations (
+            relation_id INTEGER PRIMARY KEY ,
+            relation_name VARCHAR(255) ,
+            is_client BOOLEAN ,
+            is_supplier BOOLEAN
+        )
+        """,
+        """ CREATE TABLE sales (
+                sale_id INTEGER PRIMARY KEY ,
+                relation_id INTEGER
+                )
+        """)
+    conn = None
+    try:
+        # read the connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        # create table one by one
+        for command in commands:
+            cur.execute(command)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+if __name__ == '__main__':
+    create_tables()
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79
