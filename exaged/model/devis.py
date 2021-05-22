f847eb7ebb87b20a0fe8bd010888b3597172eadd
Alex Baizeau <alexandre.baizeau@gmail.com>
2021-05-22 17:55:46 +0200

    Add new models for planning app

--- exaged/model/devis.py
+++ exaged/model/devis.py
@@ -1,14 +1,21 @@
+from datetime import datetime
+
 from exaged.model import Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
+
 class Devis(Base):
-    __tablename__ = 'devis'
+    __tablename__ = "devis"
     id = Column(Integer, primary_key=True)
     exact_quotation_id = Column(String(255), unique=True)
     exact_quotation_number = Column(Integer)
-    exact_tier_id = Column(String(255), ForeignKey('tier.exact_id'))
+    exact_tier_id = Column(String(255), ForeignKey("tier.exact_id"))
     exact_your_ref = Column(String(255))
     exact_order_description = Column(String(255))
     tier = relationship("Tier")
 
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(
+        DateTime, nullable=False, default=datetime.now, onupdate=datetime.now
+    )
--- exaged/model/tier_client.py
+++ exaged/model/tier_client.py
@@ -1,10 +1,17 @@
+from datetime import datetime
+
 from exaged.model import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, DateTime, Integer, String
 
 
 class TierClient(Base):
-    __tablename__ = 'tier_client'
+    __tablename__ = "tier_client"
     id = Column(Integer, primary_key=True)
     exact_id = Column(String(255), unique=True)
     prefixed_account_code = Column(String(255))
     exact_name = Column(String(255))
+
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(
+        DateTime, nullable=False, default=datetime.now, onupdate=datetime.now
+    )
